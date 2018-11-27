fe22886a71f42df50253c9b173086e87c36c18b4
ProfessorProspector <thedoctorsoda@gmail.com>
2018-11-27 15:43:37 -0800

    Update pomf

--- src/main/java/prospector/silk/block/SilkSaplingBlock.java
+++ src/main/java/prospector/silk/block/SilkSaplingBlock.java
@@ -1,13 +1,13 @@
 package prospector.silk.block;
 
-import net.minecraft.block.BlockSapling;
 import net.minecraft.block.Material;
+import net.minecraft.block.SaplingBlock;
 import net.minecraft.sound.BlockSoundGroup;
 import prospector.silk.util.SilkBlockBuilder;
 import prospector.silk.util.SilkSaplingGenerator;
 
-public class SilkBlockSapling extends BlockSapling {
-	public SilkBlockSapling(SilkSaplingGenerator treeGenerator) {
+public class SilkSaplingBlock extends SaplingBlock {
+	public SilkSaplingBlock(SilkSaplingGenerator treeGenerator) {
 		super(treeGenerator, SilkBlockBuilder.create(Material.PLANT).noCollision().acceptRandomTicks().breakInstantly().setSoundGroup(BlockSoundGroup.field_11535).build());
 	}
 }
