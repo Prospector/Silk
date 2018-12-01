4064ad16737453dc814caad6541a80fd3b76142d
ProfessorProspector <thedoctorsoda@gmail.com>
2018-12-01 00:08:17 -0800

    Continue Assembly dev

---
+++ src/main/java/prospector/silk/util/RenderUtil.java
@@ -0,0 +1,11 @@
+package prospector.silk.util;
+
+public class RenderUtil {
+	public static double frac(double d) {
+		return (double) 1 / (double) 16 * d;
+	}
+
+	public static float frac(float d) {
+		return (float) 1 / (float) 16 * d;
+	}
+}
