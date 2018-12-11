863d8e899b2eca038e06f1b7e7afc315879bdb17
ProfessorProspector <thedoctorsoda@gmail.com>
2018-12-10 16:15:42 -0800

    Drip definitions

---
+++ src/main/java/prospector/silk/fluid/DripValues.java
@@ -0,0 +1,13 @@
+package prospector.silk.fluid;
+
+public class DripValues {
+	// Common Equivalents
+	public static final int BLOCK = 1620;
+	public static final int INGOT = BLOCK / 9;
+	public static final int NUGGET = INGOT / 9;
+	public static final int DRIP = 1;
+
+	// Fluid Containers
+	public static final int BUCKET = BLOCK;
+	public static final int BOTTLE = BUCKET / 3;
+}
