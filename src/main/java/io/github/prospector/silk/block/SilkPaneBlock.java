cfc3f71b10fb731b051db3fa63aefa60e43d4059
ProfessorProspector <thedoctorsoda@gmail.com>
2019-01-05 14:12:12 -0800

    Begin Architectonic

---
+++ src/main/java/io/github/prospector/silk/block/SilkPaneBlock.java
@@ -0,0 +1,9 @@
+package io.github.prospector.silk.block;
+
+import net.minecraft.block.PaneBlock;
+
+public class SilkPaneBlock extends PaneBlock {
+	public SilkPaneBlock(Settings settings) {
+		super(settings);
+	}
+}
---
+++ src/main/java/io/github/prospector/silk/block/SilkStairsBlock.java
@@ -0,0 +1,10 @@
+package io.github.prospector.silk.block;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.StairsBlock;
+
+public class SilkStairsBlock extends StairsBlock {
+	public SilkStairsBlock(BlockState baseBlockState, Settings settings) {
+		super(baseBlockState, settings);
+	}
+}
