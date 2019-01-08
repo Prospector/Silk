0dfa0af0111816d3469a5f402c475cfe3a1b72ff
ProfessorProspector <thedoctorsoda@gmail.com>
2019-01-07 18:04:27 -0800

    Begin Wilderness fireflies

---
+++ src/main/java/io/github/prospector/silk/particle/SilkDefaultParticleType.java
@@ -0,0 +1,9 @@
+package io.github.prospector.silk.particle;
+
+import net.minecraft.particle.DefaultParticleType;
+
+public class SilkDefaultParticleType extends DefaultParticleType {
+	public SilkDefaultParticleType(boolean boolean_1) {
+		super(boolean_1);
+	}
+}
