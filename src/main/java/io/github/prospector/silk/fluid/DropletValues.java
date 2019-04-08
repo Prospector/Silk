28b1965f590c2dbc5eeea8abe05597d4d0c40806
Prospector <prospectordev@gmail.com>
2019-04-07 17:36:44 -0700

    Loader 0.4.0

--- src/main/java/io/github/prospector/silk/fluid/DropletValues.java
+++ src/main/java/io/github/prospector/silk/fluid/DropletValues.java
@@ -5,9 +5,12 @@ public class DropletValues {
 	public static final int BLOCK = 1620;
 	public static final int INGOT = BLOCK / 9;
 	public static final int NUGGET = INGOT / 9;
-	public static final int DRIP = 1;
+	public static final int DROPLET = 1;
+
+	@Deprecated // Refer to DROPLET ^
+	public static final int DRIP = DROPLET;
 
 	// Fluid Containers
 	public static final int BUCKET = BLOCK;
 	public static final int BOTTLE = BUCKET / 3;
-}
+}   
