a53423f77b05afabb381037da4332f8ed5bdf187
Meredith Espinosa <goclonefilms@gmail.com>
2019-04-15 01:53:03 -0700

    flip to 3240 instead if 324 because I messed up with calculations

--- src/main/java/io/github/prospector/silk/fluid/DropletValues.java
+++ src/main/java/io/github/prospector/silk/fluid/DropletValues.java
@@ -6,13 +6,13 @@ import net.minecraft.text.TranslatableTextComponent;
 
 public class DropletValues {
 	// Common Equivalents
-	public static final int BLOCK = 324;
+	public static final int BLOCK = 3240;
 	public static final int INGOT = BLOCK / 9;
 	public static final int NUGGET = INGOT / 9;
-	public static final int QUARTER_NUGGET = 1;
+	public static final int DROPLET = 1;
 
 	@Deprecated // Refer to DROPLET ^
-	public static final int DROPLET = QUARTER_NUGGET;
+	public static final int DRIP = DROPLET;
 
 	// Fluid Containers
 	public static final int BUCKET = BLOCK;
@@ -20,19 +20,20 @@ public class DropletValues {
 
 	public static TextComponent formatFluidValue(int amount) {
 		TextComponent text = new StringTextComponent("");
-		int buckets = amount / 324;
+		int buckets = amount / BLOCK;
 		if (buckets != 0) text.append(new TranslatableTextComponent("silk.fluid.value.bucket", buckets));
-		int remainder = amount % 324;
+		int remainder = amount % BLOCK;
 		if (remainder == 0) return text; else if (buckets != 0) text.append(", ");
-		int ingots = remainder / 36;
+		int ingots = remainder / INGOT;
 		if (ingots != 0) text.append(new TranslatableTextComponent("silk.fluid.value.ingot", ingots));
-		remainder = remainder % 36;
+		remainder = remainder % INGOT;
 		if (remainder == 0) return text; else if (ingots != 0) text.append(", ");
-		int nuggets = remainder / 4;
-		int quarterNuggets = (remainder % 4) * 25;
-		if (nuggets == 0 && quarterNuggets == 0) return text;
-		if (quarterNuggets == 0) text.append(new TranslatableTextComponent("silk.fluid.value.nugget", nuggets));
-		else text.append(new TranslatableTextComponent("silk.fluid.value.nugget_partial", nuggets, quarterNuggets));
+		int nuggets = remainder / NUGGET;
+		int droplets = remainder % NUGGET;
+		int nuggetFrac = (int)((droplets / (float)NUGGET) * 1000);
+		if (nuggets == 0 && droplets == 0) return text;
+		if (droplets == 0) text.append(new TranslatableTextComponent("silk.fluid.value.nugget", nuggets));
+		else text.append(new TranslatableTextComponent("silk.fluid.value.nugget_partial", nuggets, (nuggetFrac > 100)? droplets : "0"+droplets));
 		return text;
 	}
 }   
